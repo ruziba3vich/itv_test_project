03fa5759324a8a63e1257bd6bdda85e42120a58f
ruziba3vich <rruzibayevich@gmail.com>
2025-04-06 04:52:47 +0500

    feat Auth_repo: add LoginUser service

--- internal/repos/auth_repo.go
+++ internal/repos/auth_repo.go
@@ -1,11 +1,16 @@
 package repos
 
-import "context"
+import (
+	"context"
+
+	"github.com/ruziba3vich/itv_test_project/internal/types"
+)
 
 type (
 	AuthRepo interface {
 		GenerateTokens(ctx context.Context, userID uint) (string, string, error)
 		RefreshAccessToken(ctx context.Context, refreshToken string) (string, error)
 		ValidateJWT(tokenString string) (string, error)
+		LoginUser(ctx context.Context, req *types.LoginUserRequest) error
 	}
 )
