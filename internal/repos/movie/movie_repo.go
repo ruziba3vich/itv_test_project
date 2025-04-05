44a46b21b1092099fe33b3d77484be68716bd474
ruziba3vich <rruzibayevich@gmail.com>
2025-04-06 01:25:44 +0500

    fix repos: fix request and response types of methods

--- internal/repos/movie/movie_repo.go
+++ internal/repos/movie/movie_repo.go
@@ -3,13 +3,13 @@ package repos
 import (
 	"context"
 
-	"github.com/ruziba3vich/itv_test_project/internal/models"
+	"github.com/ruziba3vich/itv_test_project/internal/types"
 )
 
 type IMovieService interface {
-	Create(ctx context.Context, movie *models.Movie) error
-	GetAll(ctx context.Context) ([]models.Movie, error)
-	GetByID(ctx context.Context, id uint) (*models.Movie, error)
-	Update(ctx context.Context, id uint, movie *models.Movie) error
-	Delete(ctx context.Context, id uint) error
+	Create(ctx context.Context, req *types.CreateMovieRequest) (*types.CreateMovieResponse, error)
+	Delete(ctx context.Context, req *types.DeleteMovieRequest) (*types.DeleteMovieResponse, error)
+	GetAll(ctx context.Context, req *types.GetAllRequest) (*types.GetAllResponse, error)
+	GetByID(ctx context.Context, req *types.GetByIDRequest) (*types.GetByIDResponse, error)
+	Update(ctx context.Context, id uint, req *types.UpdateMovieRequest) (*types.UpdateMovieResponse, error)
 }
