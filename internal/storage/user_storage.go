7c8d112860c35dbd81c7b07301397c430b8d4f8c
ruziba3vich <rruzibayevich@gmail.com>
2025-04-06 02:49:23 +0500

    feat user_storage: implement user storage for user creation

---
+++ internal/storage/user_storage.go
@@ -0,0 +1,45 @@
+package storage
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/ruziba3vich/itv_test_project/internal/models"
+	"gorm.io/gorm"
+)
+
+type (
+	UserStorage struct {
+		db *gorm.DB
+	}
+)
+
+// CreateUser adds a new user to the database
+func (s *UserStorage) CreateUser(ctx context.Context, user *models.User) error {
+	existingUser, err := s.getUserByUsername(ctx, *user.Username)
+	if err != nil {
+		return fmt.Errorf("failed to verify username availability: %s", err.Error())
+	}
+	if existingUser != nil {
+		return fmt.Errorf("this username is already taken")
+	}
+	return s.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		if err := tx.Create(user).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+// GetUserByUsername retrieves a user by username
+func (s *UserStorage) getUserByUsername(ctx context.Context, username string) (*models.User, error) {
+	var user models.User
+	if err := s.db.WithContext(ctx).Where("username = ?", username).First(&user).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil // User not found
+		}
+		return nil, err
+	}
+	return &user, nil
+}
