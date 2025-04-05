82fa4717ac32fbca25558d18ccd6353ee91515d4
ruziba3vich <rruzibayevich@gmail.com>
2025-04-06 01:31:47 +0500

    feat db: create db connection

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,1 +1,32 @@
 package db
+
+import (
+	"fmt"
+
+	"github.com/ruziba3vich/itv_test_project/internal/models"
+	"github.com/ruziba3vich/itv_test_project/pkg/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// NewDB initializes a new GORM database connection using the provided config
+func NewDB(cfg *config.DBConfig) (*gorm.DB, error) {
+	// Construct the DSN from the config
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		cfg.Host, cfg.User, cfg.Password, cfg.DBName, cfg.Port, cfg.SSLMode,
+	)
+
+	// Open the database connection
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %v", err)
+	}
+
+	// Auto-migrate the schema
+	if err := db.AutoMigrate(&models.Movie{}); err != nil {
+		return nil, fmt.Errorf("failed to migrate database: %v", err)
+	}
+
+	return db, nil
+}
