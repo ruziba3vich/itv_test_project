4f88b86f989af4feb334cdc29bc5bf80b7f99967
ruziba3vich <rruzibayevich@gmail.com>
2025-04-06 05:08:19 +0500

    feat auth_repo: add RegisterUser

--- internal/repos/auth_repo.go
+++ internal/repos/auth_repo.go
@@ -3,6 +3,7 @@ package repos
 import (
 	"context"
 
+	"github.com/ruziba3vich/itv_test_project/internal/models"
 	"github.com/ruziba3vich/itv_test_project/internal/types"
 )
 
@@ -12,5 +13,6 @@ type (
 		RefreshAccessToken(ctx context.Context, refreshToken string) (string, error)
 		ValidateJWT(tokenString string) (string, error)
 		LoginUser(ctx context.Context, req *types.LoginUserRequest) (uint, error)
+		RegisterUser(ctx context.Context, user *models.User) error
 	}
 )
